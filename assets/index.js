7fb9bdbcf68a495c36852072e58973e719e45bda
Michael Huensch <setup@run00.com>
2017-11-08 19:35:11 -0800

    Added logging

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('content loaded!')
   var header = document.getElementsByClassName('page-header')[0]
-  console.log('HEADER', header)
   var child = null
 
   for (var i = 0; i < header.childNodes.length; i++) {
     child = header.childNodes[i]
     if (child.className !== 'project-tagline' && child.className === 'btn') continue;
+    console.log('CHILD', child)
     child.parentNode.removeChild(child)
   }
 
