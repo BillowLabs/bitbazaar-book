06644cba7917a7ec88cbbf238e799e1eeef050f8
Michael Huensch <setup@run00.com>
2017-11-08 19:29:39 -0800

    Added logging

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('content loaded!')
   var header = document.getElementsByClassName('page-header')[0]
+  console.log('HEADER', header)
   var child = null
 
   for (var i = 0; i < header.childNodes.length; i++) {
