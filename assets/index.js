be3e5388874b888ac150997cef048b33733132f6
Michael Huensch <setup@run00.com>
2017-11-09 23:44:08 -0600

    Changed js reformat to css reformat

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('content loaded!')
-  var header = document.getElementsByClassName('page-header')[0]
-  var child = null
-
-  for (var i = 0; i < header.childNodes.length; i++) {
-    child = header.childNodes[i]
-    if (child.className !== 'project-tagline' && child.className !== 'btn') continue
-    header.removeChild(child)
-  }
+  // var header = document.getElementsByClassName('page-header')[0]
+  // var child = null
+  //
+  // for (var i = 0; i < header.childNodes.length; i++) {
+  //   child = header.childNodes[i]
+  //   if (child.className !== 'project-tagline' && child.className !== 'btn') continue
+  //   header.removeChild(child)
+  // }
 
 })
