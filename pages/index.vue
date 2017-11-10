6479685cde7eb94cb5a6221e040b620a766e7bc5
Michael Huensch <setup@run00.com>
2017-11-10 14:19:20 -0600

    Added test link to home

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,8 @@
       <h2 class="subtitle">
         Nuxt.js project
       </h2>
+
+      <router-link :to="{ name: 'chapters' }">Go to Chapters</router-link>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
